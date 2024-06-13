100b6ff6e5ed61761260422cb2f0c1821143820a
Otaiki1 <otaikisadiq2804@gmail.com>
2024-06-13 14:31:33 +0100

    feat: Initial Commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-custom-image bg-cover bg-center h-screen">
+      <h1>Welcome</h1>
     </div>
   );
 }
